62c3579a58dc7c5942cc64143709ea29ee36b2f9
rallosdrake <jimbobbarlow74@gmail.com>
2022-05-03 14:18:11 +0100

    this commit adds the following components, Header, Log-in, log out, new char and signup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { Routes, Route } from 'react-router-dom';
-import './css/App.css';
-
+import { Routes, Route } from "react-router-dom";
+import "./css/App.css";
+import Header from "./Components/Header";
+import LogInLogOut from "./Components/Log-in-out";
+import Signup from "./Components/Signup";
+import Login from "./Components/Login";
+import NewCharacter from "./Components/NewCharacter";
 function App() {
-	return (
-		<div className='App'>
-			<header className='App-header'>Welcome to dndinder!</header>
-			<Routes>
-				<Route path='/' />
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Header />
+      <LogInLogOut />
+      <Signup />
+      <Login />
+
+      <NewCharacter />
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
