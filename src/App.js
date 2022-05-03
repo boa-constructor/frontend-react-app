15369ec9138bfbd6bc6ea267cd2a1b4aa22dac8a
amcombes <amcombes@penguin>
2022-05-03 10:05:57 +0100

    organises files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'></header>
+			<header className='App-header'>Welcome to dndinder!</header>
+			<Routes>
+				<Route path='/' />
+			</Routes>
 		</div>
 	);
 }
