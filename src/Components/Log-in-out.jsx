7d0f09f78b7a0281f6682234c9307e7f8e2a61cf
rallosdrake <jimbobbarlow74@gmail.com>
2022-05-03 15:09:23 +0100

    this commit updates the components

---
+++ src/Components/Log-in-out.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LogInLogOut = () => {
+  return (
+    <div className="LogInLogOut">
+      <button type="button" className="LogInButton">
+        Log-in
+      </button>
+      <button type="button" className="LogOutButton">
+        Log-out
+      </button>
+    </div>
+  );
+};
+
+export default LogInLogOut;
---
+++ src/Components/NewCharButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewCharButton = () => {
+  return (
+    <div className="NewCharButton">
+      <button>Create New Character</button>
+    </div>
+  );
+};
+
+export default NewCharButton;
