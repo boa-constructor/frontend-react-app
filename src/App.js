9167e069e1e64fe90851daf56d466f666b578b50
rallosdrake <jimbobbarlow74@gmail.com>
2022-05-04 10:38:18 +0100

    this commit adds a nav bar which houses loginlogout login newewcharbutton and signup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import "./css/App.css";
 import Header from "./Components/Header";
-import LogInLogOut from "./Components/Log-in-out";
-import Signup from "./Components/Signup";
-import Login from "./Components/Login";
-import NewCharacter from "./Components/NewCharacter";
 import NewCharButton from "./Components/NewCharButton";
 import SlimCharCards from "./Components/SlimCharCards";
 import CharacterProfile from "./Components/CharacterProfile";
 import UserProfile from "./Components/Profile";
+import Nav from "./Components/Nav";
 function App() {
   return (
     <div className="App">
       <Header />
-      <LogInLogOut />
-      <Signup />
-      <Login />
-      <NewCharButton />
-      <NewCharacter />
+      <Nav />
       <SlimCharCards />
       <CharacterProfile />
       <UserProfile />
