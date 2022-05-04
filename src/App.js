2322eb76ea7707867a3e690a94cf25189885d577
rallosdrake <jimbobbarlow74@gmail.com>
2022-05-04 15:11:23 +0100

    this commit adds the start of google auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import "./css/App.css";
 import Header from "./Components/Header";
-import NewCharButton from "./Components/NewCharButton";
 import SlimCharCards from "./Components/SlimCharCards";
 import CharacterProfile from "./Components/CharacterProfile";
 import UserProfile from "./Components/Profile";
 import Nav from "./Components/Nav";
-import SignUpPage from "./Components/Signuppage";
+import SignUpPage from "./Components/SignUpPage";
 function App() {
   return (
     <div className="App">
