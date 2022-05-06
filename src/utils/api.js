0483de45617f34bfcb1febf4d30cce3de2ac99d8
rallosdrake <jimbobbarlow74@gmail.com>
2022-05-06 15:49:36 +0100

    this commit is to allow routes

--- src/utils/api.js
+++ src/utils/api.js
@@ -21,19 +21,3 @@ export const postUserProfile = async (username) => {
     console.log(err);
   }
 };
-
-// export const postUserProfile = (user) => {
-//   return fetch(`http://127.0.0.1:5001/dndinder-68dcc/us-central1/addUser`, {
-//     method: "POST",
-//     headers: {
-//       Accept: "application/json",
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({ username: user.user }),
-//   })
-//     .then((res) => res.json())
-
-//     .catch((error) => {
-//       console.error(error);
-//     });
-// };
