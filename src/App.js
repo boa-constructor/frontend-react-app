f44ef474b990d90771ad15abe44fae33f1dcbec9
rallosdrake <jimbobbarlow74@gmail.com>
2022-05-09 12:52:09 +0100

    this commit adds our form variables to our api

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import { initializeApp } from "firebase/app";
-import { Routes, Route } from "react-router-dom";
-import "./css/App.css";
-import Header from "./Components/Header";
-import SlimCharCards from "./Components/SlimCharCards";
-import CharacterProfile from "./Components/CharacterProfile";
-import SetProfile from "./Components/Profile";
-import Nav from "./Components/Nav";
-import SignUpPage from "./Components/SignUpPage";
-import { UserContext } from "./contexts/user";
-import { useState } from "react";
+import { initializeApp } from 'firebase/app';
+import { Routes, Route } from 'react-router-dom';
+import './css/App.css';
+import Header from './Components/Header';
+import SlimCharCards from './Components/SlimCharCards';
+import CharacterProfile from './Components/CharacterProfile';
+import EditProfile from './Components/EditProfile';
+import Nav from './Components/Nav';
+import SignUpPage from './Components/SignUpPage';
+import { UserContext } from './contexts/user';
+import { useState } from 'react';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB69WIWau0OsUGMqTPDA5jJs6NMsEncGR4",
-  authDomain: "dndinder-68dcc.firebaseapp.com",
+  apiKey: 'AIzaSyB69WIWau0OsUGMqTPDA5jJs6NMsEncGR4',
+  authDomain: 'dndinder-68dcc.firebaseapp.com',
   databaseURL:
-    "https://dndinder-68dcc-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "dndinder-68dcc",
-  storageBucket: "dndinder-68dcc.appspot.com",
-  messagingSenderId: "887332428606",
-  appId: "1:887332428606:web:fd999c4c18be4c0d106a6f",
-  measurementId: "G-V4QQMW9LBW",
+    'https://dndinder-68dcc-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'dndinder-68dcc',
+  storageBucket: 'dndinder-68dcc.appspot.com',
+  messagingSenderId: '887332428606',
+  appId: '1:887332428606:web:fd999c4c18be4c0d106a6f',
+  measurementId: 'G-V4QQMW9LBW',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 function App() {
-  const [user, setUser] = useState(localStorage.getItem("user"));
+  const [user, setUser] = useState(localStorage.getItem('user'));
   return (
     <UserContext.Provider value={{ user, setUser }}>
       <div className="App">
@@ -40,7 +40,7 @@ function App() {
         <SignUpPage />
         <SlimCharCards />
         <CharacterProfile />
-        <SetProfile user={user} setProfile={setUser} />
+        <EditProfile user={user} setProfile={setUser} />
         <Routes>
           <Route path="/" />
         </Routes>
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,22 +1,35 @@
-import axios from "axios";
+import axios from 'axios';
 const firestoreTestApi = axios.create({
-  baseURL: "http://127.0.0.1:5001/dndinder-68dcc/us-central1",
+  baseURL: 'http://127.0.0.1:5001/dndinder-68dcc/us-central1',
 });
 
-export const postUserProfile = async (username, about_me) => {
+export const postUserProfile = async (
+  username,
+  avatar_url,
+  years_played,
+  about_me,
+  Online,
+  Face_to_face,
+  is_dm
+) => {
   console.log(username);
   const config = {
     headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
     },
   };
   try {
     const { data } = await firestoreTestApi.post(`/addUser`, {
       username,
+      avatar_url,
+      years_played,
       about_me,
+      Online,
+      Face_to_face,
+      is_dm,
     });
-    console.log(data, "this is data");
+    console.log(data, 'this is data');
     return data;
   } catch (err) {
     console.log(err);
