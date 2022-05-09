3f7fdd198d5bbe49f2fb4c445d50dcc9dd56a43c
rallosdrake <jimbobbarlow74@gmail.com>
2022-05-09 14:52:28 +0100

    This commit finises off the userprofile page

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,15 +3,7 @@ const firestoreTestApi = axios.create({
   baseURL: 'http://127.0.0.1:5001/dndinder-68dcc/us-central1',
 });
 
-export const postUserProfile = async (
-  username,
-  avatar_url,
-  years_played,
-  about_me,
-  Online,
-  Face_to_face,
-  is_dm
-) => {
+export const postUserProfile = async (username, about_me) => {
   console.log(username);
   const config = {
     headers: {
@@ -22,14 +14,20 @@ export const postUserProfile = async (
   try {
     const { data } = await firestoreTestApi.post(`/addUser`, {
       username,
-      avatar_url,
-      years_played,
       about_me,
-      Online,
-      Face_to_face,
-      is_dm,
     });
     console.log(data, 'this is data');
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+export const updateUserProfile = async (postBody, user_id) => {
+  const patchBody = { ...postBody };
+  try {
+    const { data } = await firestoreTestApi.patch(`/updateUser/${user_id}`, {
+      patchBody,
+    });
     return data;
   } catch (err) {
     console.log(err);
