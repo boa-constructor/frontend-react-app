f3cc1792f16fba7603fd16d0cbd1e802d6a84ac6
S Roze <sherozem4@gmail.com>
2022-05-10 14:12:11 +0100

    displayCharacters rendered and organised

--- src/Components/Character.jsx
+++ src/Components/Character.jsx
@@ -1,20 +1,45 @@
-import { useParams } from "react-router-dom"
-import {useState, useEffect} from "react"
-import {getCharacterByID} from "../utils/api"
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getCharacterByID } from "../utils/api";
 const Character = (req, res) => {
-    const {character_id} = useParams()
-    const [character, setCharacter] = useState({})
+  const { character_id } = useParams();
+  const [character, setCharacter] = useState({});
 
-    useEffect(() => {
-        getCharacterByID(character_id).then((data) => {
-            console.log(data)
-            setCharacter(data)
-        }).catch((err) => {
-            console.log(err)
-        })
-    }, [character_id])
+  useEffect(() => {
+    getCharacterByID(character_id)
+      .then((data) => {
+        console.log(data);
+        setCharacter(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [character_id]);
+  console.log(character.background);
 
-    return <p>I am character {character.character_name}</p>
-}
+  const games_played = character.games_played;
+  let index = 0;
 
-export default Character
+  return (
+    <div className="character-box">
+      <div className="character-intro">
+        <div className="character-img">
+          <img src={character.avatar} alt="avatar for character" />
+        </div>
+
+        <div className="character-details">
+          <div>{character.character_name}</div>
+          <div>Class: {character.class}</div>
+          {games_played &&
+            games_played.map((games) => {
+              return <div key={index++}>{games}</div>;
+            })}
+        </div>
+      </div>
+
+      <div className="character-background">Hey {character.background}</div>
+    </div>
+  );
+};
+
+export default Character;
