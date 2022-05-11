f2f14da6cd1a1814a4cfb2f57e9db06824897678
rallosdrake <jimbobbarlow74@gmail.com>
2022-05-11 16:46:05 +0100

    this commit adds styling to the create char and edit profile form

--- src/Components/UserProfile.jsx
+++ src/Components/UserProfile.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useContext } from 'react';
 
-import { getCharacterByID, getUserProfile } from '../utils/api';
+import { getUserProfile } from '../utils/api';
 import { UserContext } from '../contexts/user';
 import { Link } from 'react-router-dom';
 import GetCharacterByID from './GetCharacterByID';
@@ -32,72 +32,52 @@ const UserProfile = () => {
   let preferred_days = preferences.days;
 
   return (
-    <div>
+    <div className="your_profile">
       <h2>Welcome to your profile {userProfile.username}</h2>
-
       <Link to="/CreateCharacter" className="Link">
-        Add Character
+        <p>Add Character</p>
       </Link>
-      <br></br>
       <Link to="/EditProfile" className="Link">
-        Edit Profile
+        <p>Edit Profile</p>
       </Link>
-
-      <div className="user-profile">
-        <div className="user-intro">
-          <div className="avatar">
-            <img src={userProfile.avatar_url} alt="avatar" />
-          </div>
-
-          <div className="user-details">
-            <div>{userProfile.username}</div>
-            <div>{userProfile.name}</div>
-            <div>{userProfile.years_played}</div>
-            <div>DM: {userProfile.is_dm ? 'Yes' : 'No'}</div>
-          </div>
-        </div>
-
-        <div className="characters-preferences">
-          <div className="characters">
-            <p>Characters:</p>
-            <ul>
-              {characterID_Array &&
-                characterID_Array.map((id) => {
-                  return <GetCharacterByID key={id} id={id} />;
-                })}
-            </ul>
-          </div>
-
-          <div className="preferences">
-            <div className="preferred-days">
-              <ul>
-                Preferred Days to play:
-                {preferred_days &&
-                  preferred_days.map((day) => {
-                    return <li key={index++}>{day}</li>;
-                  })}
-              </ul>
-            </div>
-
-            <div className="play-preference">
-              <div>Play Online: {preferences.play_online ? 'Yes' : 'No'} </div>
-              <div>Play Offline:{preferences.play_online ? 'Yes' : 'No'}</div>
-            </div>
-          </div>
-        </div>
-
-        <div className="about-connections">
-          <div className="about-me">About Me:{userProfile.about_me}</div>
-          <div className="connections">
-            <p>Connections:</p>
-            <ul>
-              {connections &&
-                connections.map((connection) => {
-                  return <li key={index++}>{connection}</li>;
-                })}
-            </ul>
-          </div>
-        </div>
+      <img src={userProfile.avatar_url} alt="avatar" />
+      <div className="user-details">
+        {userProfile.username}
+        {userProfile.name}
+        {userProfile.years_played}
+        DM: {userProfile.is_dm ? 'Yes' : 'No'}
+      </div>
+      <div className="characters">
+        <p>Characters:</p>
+        <ul>
+          {characterID_Array &&
+            characterID_Array.map((id) => {
+              return <GetCharacterByID key={id} id={id} />;
+            })}
+        </ul>
+      </div>
+      <div className="preferred-days">
+        <ul>
+          Preferred Days to play:
+          {preferred_days &&
+            preferred_days.map((day) => {
+              return <li key={index++}>{day}</li>;
+            })}
+        </ul>
+      </div>
+      <div className="play-preference">
+        Play Online: {preferences.play_online ? 'Yes' : 'No'}
+        Play Offline:{preferences.play_online ? 'Yes' : 'No'}
+      </div>
+      <div className="about-connections">
+        About Me:{userProfile.about_me}
+        <p>Connections:</p>
+        <ul>
+          {connections &&
+            connections.map((connection) => {
+              return <li key={index++}>{connection}</li>;
+            })}
+        </ul>
       </div>
     </div>
   );
