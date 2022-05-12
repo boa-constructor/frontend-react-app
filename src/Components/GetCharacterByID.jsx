3b41d43591540bb76e2e0231a0b2e83da116f41e
rallosdrake <jimbobbarlow74@gmail.com>
2022-05-12 11:29:04 +0100

    small changes to indi char page to fix about me

--- src/Components/GetCharacterByID.jsx
+++ src/Components/GetCharacterByID.jsx
@@ -23,6 +23,7 @@ const GetCharacterByID = (props) => {
           <Link to={`/characters/${props.id}`} className="Link">
             {character.character_name}
           </Link>
+          <p>test</p>
           <br></br> Class: {character.class}
           <br></br>
           <img
