9c984e75daa8ef5146c31d7dd4ed21a3c20065bc
amcombes <amcombes@penguin>
2022-05-12 10:36:34 +0100

    pre-populates edit profile with user data

--- src/Components/UserProfile.jsx
+++ src/Components/UserProfile.jsx
@@ -6,69 +6,68 @@ import { Link } from 'react-router-dom';
 import GetCharacterByID from './GetCharacterByID';
 
 const UserProfile = () => {
-  const [userProfile, setUserProfile] = useState({});
-  const { user } = useContext(UserContext);
+	const [userProfile, setUserProfile] = useState({});
+	const { user } = useContext(UserContext);
 
-  let index = 0;
+	let index = 0;
 
-  useEffect(() => {
-    getUserProfile(user)
-      .then((data) => {
-        setUserProfile(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [user]);
-  console.log(userProfile)
+	useEffect(() => {
+		getUserProfile(user)
+			.then((data) => {
+				setUserProfile(data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, [user]);
 
-  const characterID_Array = userProfile.characters;
+	const characterID_Array = userProfile.characters;
 
-  // let preferences = {};
-  // for (const key in userProfile.preferences) {
-  //   preferences[key] = userProfile.preferences[key];
-  // }
+	// let preferences = {};
+	// for (const key in userProfile.preferences) {
+	//   preferences[key] = userProfile.preferences[key];
+	// }
 
-  // let preferred_days = preferences.days;
+	// let preferred_days = preferences.days;
 
-  return (
-    <div>
-      <h2>Welcome to your profile {userProfile.username}</h2>
+	return (
+		<div>
+			<h2>Welcome to your profile {userProfile.username}</h2>
 
-      <Link to="/CreateCharacter" className="Link">
-        Add Character
-      </Link>
-      <br></br>
-      <Link to="/EditProfile" className="Link">
-        Edit Profile
-      </Link>
+			<Link to='/CreateCharacter' className='Link'>
+				Add Character
+			</Link>
+			<br></br>
+			<Link to='/EditProfile' className='Link'>
+				Edit Profile
+			</Link>
 
-      <div className="user-profile">
-        <div className="user-intro">
-          <div className="avatar">
-            <img src={userProfile.avatar_url} alt="avatar" />
-          </div>
+			<div className='user-profile'>
+				<div className='user-intro'>
+					<div className='avatar'>
+						<img src={userProfile.avatar_url} alt='avatar' />
+					</div>
 
-          <div className="user-details">
-            <div>{userProfile.username}</div>
-            <div>Years Played: {userProfile.years_played}</div>
-            <div>DM: {userProfile.is_dm === "true" ? 'Yes' : 'No'}</div>
-          </div>
-        </div>
+					<div className='user-details'>
+						<div>{userProfile.username}</div>
+						<div>Years Played: {userProfile.years_played}</div>
+						<div>DM: {userProfile.is_dm === 'true' ? 'Yes' : 'No'}</div>
+					</div>
+				</div>
 
-        <div className="characters-preferences">
-          <div className="characters">
-            <p>Characters:</p>
-            <ul>
-              {characterID_Array &&
-                characterID_Array.map((id) => {
-                  return <GetCharacterByID key={id} id={id} />;
-                })}
-            </ul>
-          </div>
+				<div className='characters-preferences'>
+					<div className='characters'>
+						<p>Characters:</p>
+						<ul>
+							{characterID_Array &&
+								characterID_Array.map((id) => {
+									return <GetCharacterByID key={id} id={id} />;
+								})}
+						</ul>
+					</div>
 
-          <div className="preferences">
-            {/* <div className="preferred-days">
+					<div className='preferences'>
+						{/* <div className="preferred-days">
               <ul>
                 Preferred Days to play:
                 {preferred_days &&
@@ -78,16 +77,20 @@ const UserProfile = () => {
               </ul>
             </div> */}
 
-            <div className="play-preference">
-              <p>Play Online: {userProfile.play_online ? 'Yes' : 'No'} </p>
-              <p>Play Offline: {userProfile.play_online ? 'Yes' : 'No'}</p>
-            </div>
-          </div>
-        </div>
+						<div className='play-preference'>
+							<p>
+								Play Online: {userProfile.play_online === 'true' ? 'Yes' : 'No'}{' '}
+							</p>
+							<p>
+								Play Offline:{' '}
+								{userProfile.play_online === 'true' ? 'Yes' : 'No'}
+							</p>
+						</div>
+					</div>
+				</div>
 
-        
-          <p className="about-me">About Me: {userProfile.about_me}</p>
-          {/* <div className="connections">
+				<p className='about-me'>About Me: {userProfile.about_me}</p>
+				{/* <div className="connections">
             {/* <p>Connections:</p>
             <ul>
               {connections &&
@@ -96,9 +99,9 @@ const UserProfile = () => {
                 })}
             </ul> 
           </div> */}
-        </div>
-    </div>
-  );
+			</div>
+		</div>
+	);
 };
 
 export default UserProfile;
