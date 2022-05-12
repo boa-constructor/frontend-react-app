b45dbf0396ee90aba0f02a31c9e403b635cf0e12
S Roze <sherozem4@gmail.com>
2022-05-12 11:39:43 +0100

    navbar updated

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,21 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 const NavBar = () => {
   return (
-    <details>
-      <summary></summary>
-      <nav className="menu">
-        <Link to="/" className="Link NavLink">
-          Home
-        </Link>{' '}
-        <Link to="/Profile" className="Link NavLink">
-          Profile
-        </Link>{' '}
-        <Link to="/groups" className="Link NavLink">
-          {' '}
-          Groups
-        </Link>{' '}
-      </nav>
-    </details>
+    <nav className="menu">
+      <Link to="/" className="Link NavLink btn">
+        Home
+      </Link>{' '}
+      <Link to="/Profile" className="Link NavLink btn">
+        Profile
+      </Link>{' '}
+      <Link to="/groups" className="Link NavLink btn">
+        {' '}
+        Groups
+      </Link>{' '}
+    </nav>
   );
 };
 
